96d79919e1fec54068c7d94f41cafdec8bdb0b06
Noah Benveniste <benven.noa1995@gmail.com>
2024-04-23 15:57:58 -0400

    Successfully deployed web app to Azure. Made some changes to index file including adding a static image and some additional text.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Winnie is stinky!!!' });
 });
 
 module.exports = router;
